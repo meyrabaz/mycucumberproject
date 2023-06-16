4d480621df3306ec4ddbc6fc81724d809ae10e4f
Meyra Baz <123989144+meyrabaz@users.noreply.github.com>
2023-06-17 00:02:00 +0300

    17.06.2023..

---
+++ src/test/java/stepdefinitions/CommonStepDefs.java
@@ -0,0 +1,35 @@
+package stepdefinitions;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import pages.GooglePage;
+import utilities.Driver;
+import utilities.MediaUtils;
+
+import java.io.IOException;
+
+public class CommonStepDefs {
+    @Then("close the application")
+    public void close_the_application() {
+        Driver.closeDriver();
+    }
+    @Given("user navigates to {string}")
+    public void user_navigates_to(String string) {
+        Driver.getDriver().get(string);
+        try{
+            GooglePage googlePage = new GooglePage();
+            googlePage.popUpAccept.click();//If there is a pop up on google clicking
+        }catch (Exception e){
+        }
+    }
+    @Then("verify the page title contains {string}")
+    public void verify_the_page_title_contains(String string) {
+        Assert.assertTrue(Driver.getDriver().getTitle().contains(string));
+    }
+
+    @And("capture the screenshot")
+    public void captureTheScreenshot() throws IOException {
+        MediaUtils.takeScreenshotOfTheEntirePage();
+    }
+}
