de3d6d21021ec400cd6b17383e378a6e6dfc5f00
Meyra Baz <123989144+meyrabaz@users.noreply.github.com>
2023-07-01 16:48:15 +0300

    1.07.2023......

--- src/test/java/runners/SmokeTestRunner.java
+++ src/test/java/runners/SmokeTestRunner.java
@@ -10,14 +10,14 @@ import org.junit.runner.RunWith;
                 "html:target/default-cucumber-reports.html",
                 "json:target/json-reports/cucumber.json",
                 "junit:target/xml-report/cucumber.xml",
-                "rerun:target/failedRerun.txt",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+                "rerun:target/failedRerun.txt"
         },
         monochrome = true,
         features = "./src/test/resources/features",//PATH OF FEATURES FOLDER
-        glue = {"stepdefinitions","hooks"},//PATH OF STEPDEFINITIONS FOLDER
+        glue = {"stepdefinitions"},//PATH OF STEPDEFINITIONS FOLDER
         dryRun = false,//dryRun = true : RUNS AND GENERATES ONLY MISSING STEP DEFS. dryRun = false: RUN NORMAL. THIS IS DEFAULT VALUE
-        tags = "@new_user_create"
+        tags = "@smoke_test_runner"
 )
-public class Runner {
+public class SmokeTestRunner {
+
 }
